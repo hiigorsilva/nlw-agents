690f43f1fdfb410efecb20c183df41af9bff5073
hiigorsilva <higorscontato@gmail.com>
2025-07-10 16:35:50 -0300

    feat: add db seed

---
+++ src/db/seed.ts
@@ -0,0 +1,26 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+const seedDatabase = async () => {
+  try {
+    await reset(db, schema)
+
+    await seed(db, schema).refine(f => ({
+      rooms: {
+        count: 20,
+        columns: {
+          name: f.companyName(),
+          description: f.loremIpsum(),
+        },
+      },
+    }))
+
+    await sql.end()
+    console.log('🎉 Database seeded!')
+  } catch (err) {
+    console.error('🚨 SEEDING_DATABASE_ERROR', err)
+  }
+}
+
+await seedDatabase()
