4f3adc1992e4a661719aa4b224d754f457484a33
hiigorsilva <higorscontato@gmail.com>
2025-07-17 18:53:01 -0300

    feat: add health check route to monitor server status

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
+import { checkHealthRoute } from './check-health.ts'
 import { getRoomsRoute } from './get-rooms.ts'
 
 export const registerRoutes = (app: FastifyInstance) => {
+  app.register(checkHealthRoute)
   app.register(getRoomsRoute)
 }
